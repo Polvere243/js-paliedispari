47bf755e9a1d2622561797ca8c99fd234cc55d7a
Polvere243 <danielercoli@gmail.com>
2024-01-13 00:30:07 +0100

    created a function for the exercise

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,23 @@
-console.log("JS OK");
+
+/**
+ * It checks whether a word is palindrome or not
+ * @param {string} text1 is the word to check
+ */
+
+function isPalindrome (text1) {
+    let text2 = "";
+    for (let i = text1.length - 1; i >= 0; i--){
+        text2 += text1[i];
+    }
+
+    text2.toLowerCase() === text1.toLowerCase() ? paragraph.innerText = "La parola è palindroma" : paragraph.innerText = "La parola non è palindroma";
+    
+}
+
+
+
+
+
+function createRandomNumber (num1, num2) {
+    const randomNumber = Math.floor(Math.randome * (num2))+1;
+}
