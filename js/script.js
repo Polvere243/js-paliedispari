3479e03ba070fbf83ca17f53bc185a8e98b6ae29
Polvere243 <danielercoli@gmail.com>
2024-01-13 00:29:49 +0100

    did the first part of the exercise

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
 // usare un prompt per chiedere una parola all'utente
-const userWord = prompt("Inserisci una parola").trim();
+const input = document.querySelector("input");
+const checkButton = document.querySelector(".btn");
+const paragraph = document.querySelector("p");
+
+checkButton.addEventListener ("click", function() {
+    const userWord = input.value.trim();
+        if (!userWord || userWord.length < 0) {
+            console.error("Devi inserire una parola!");
+            return;
+        }
+        isPalindrome(userWord);
+}) 
+
+
+    
+
+    
+        
+    
+
+
  
+
+
+
+
+/* Secondo esercizio */
+
+/* const userChoice = prompt("Pari o dispari?").trim();
+
+const userNumber = parseInt(prompt("Scegli un numero da 1 a 5"));
+ */
+
+
