60358cebc8cab7e9020363a541e8681c29e95924
Polvere243 <danielercoli@gmail.com>
2024-01-14 11:38:59 +0100

    finished the first exercise

--- js/script.js
+++ js/script.js
@@ -1,35 +1,50 @@
-// usare un prompt per chiedere una parola all'utente
+// recupero gli elementi dal DOM
+const form = document.querySelector("form");
 const input = document.querySelector("input");
-const checkButton = document.querySelector(".btn");
 const paragraph = document.querySelector("p");
-
-checkButton.addEventListener ("click", function() {
+console.log(input);
+// al click sul bottone recupero una parola scelta dall'utente
+form.addEventListener ("submit", function(event) {
+    event.preventDefault();
     const userWord = input.value.trim();
+    console.log(userWord);
+    // controllo che la parola sia valida
         if (!userWord || userWord.length < 0) {
-            console.error("Devi inserire una parola!");
+            alert("Devi inserire una parola!");
             return;
         }
-        isPalindrome(userWord);
+    // invoco la funzione per determinare se la parola sia palindroma o no e costruisco il messaggio per il risultato
+        const adverb = isPalindrome(userWord) ? " " : "non";
+        const message = `La parola ${userWord} ${adverb} è palindroma`;
+        paragraph.innerText = message;
 }) 
 
+/* // sottopongo la scelta tra pari e dispari all'utente
 
-    
+const userChoice = prompt("Pari o dispari?");
 
-    
-        
-    
+// chiedo all'utente di inserire un numero da 1 a 5
 
+const userNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
 
- 
+// invoco la funzione per generare un numero casuale tra 1 e 5 per il computer
 
+const cpuNumber = createRandomNumber(1, 5);
 
+console.log(cpuNumber);
+// sommo il numero scelto dall'utente con quello estratto per il computer
 
+const sum = userNumber + cpuNumber;
+console.log(sum);
+// invoco la funzione per controllare se la somma sia pari o dispari
 
-/* Secondo esercizio */
-
-/* const userChoice = prompt("Pari o dispari?").trim();
-
-const userNumber = parseInt(prompt("Scegli un numero da 1 a 5"));
+isEven (sum);
+console.log(sum);
+let message = "Il vincitore è ";
+let isNumberEven;
+if (isNumberEven = true) {
+    console.log(message += "l'utente.");
+} else {
+    console.log(message += "il computer.");
+}
  */
-
-
